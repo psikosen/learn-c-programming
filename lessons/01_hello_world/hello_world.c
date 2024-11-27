d1c6e71304ae546530d50caa030df4a1eb71c159
Rook <raymondgonzalez22@gmail.com>
2024-11-27 12:13:14 -0500

    Add first lesson: Hello World

---
+++ lessons/01_hello_world/hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* This is our first C program!
+   It prints a friendly greeting to the world */
+
+int main() {
+    // Print our greeting
+    printf("Hello, World!\n");
+    
+    // Print an extra friendly message
+    printf("Welcome to the exciting world of C programming!\n");
+    
+    return 0;
+}
